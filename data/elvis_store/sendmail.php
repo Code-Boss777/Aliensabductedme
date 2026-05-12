f2154fed9bc9327ab4a25bbcd3edee557afd54ea
BossCode777 <13@mail.ru>
2026-05-12 16:38:39 +0500

    добавлен возврат формы при false для if

--- data/elvis_store/sendmail.php
+++ data/elvis_store/sendmail.php
@@ -16,6 +16,7 @@ $from = 'itunes-174@mail.ru';
 $subject = $_POST['subject'];
 $_text = $_POST['elvismail'];
 $dsn = "mysql:host=$host;dbname=$db_name;charset=$charset";
+$output_form = false;
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -27,8 +28,12 @@ try {
     die("Ошибка подключения: " . $e->getMessage());
 }
 //так же используют в запросах DELETE и тд.
+//функции empty проверяет содержит ли переменная какие либо данные 
+//функция isset проверяет присвоено ли какое либо значение переменной
 if(!empty($subject)){
     if(!empty($text)){
+//сократить объем кода можн при помощи логических операторов AND(&&), OR(||)
+// if((!empty($subject))&&(!empty($text)))
 $sql = "SELECT first_name, last_name, email FROM email_list";
 $stmt = $pdo->query($sql);
 //fetch вызов данных строки
@@ -41,7 +46,11 @@ while ($row = $stmt->fetch()){
     echo $row['first_name'] . ' ' . $row['last_name'] . ' ' . $row['email'] . '<br />';
 }
     }
-};
+}
+// else выполнится, если if получит false
+else {
+    echo "no data for send" <br/>; 
+}
 //управляющая конструкция if, формируем для исключения пустых писем
 
 // if($subject == '') {
@@ -64,13 +73,16 @@ while ($row = $stmt->fetch()){
 // echo "no reaection";
 //     }
 // };
-
-
-
-
-
-
-
-
-
+if ($output_form){
+    ?>
+    <form method="post" action="../elvis_store/sendmail.php">
+<label for="subject">Email Theme</label>
+<input type="text" name="subject" id="subject" /><br/>
+<label for="elvismail">Email data</label>
+<textarea name="elvismail" id="elvismail" rows="8" cols="60"></textarea><br/>
+<input type="submit" name="submit" value="Sender">
+    </form>
+    <?php
+}
 ?>
+
